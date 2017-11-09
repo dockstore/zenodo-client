9c7ebe19583c70f79c182de4b85fd27df5f406f1
Denis Yuen <denis.yuen@oicr.on.ca>
2017-11-09 16:47:59 -0500

    Initial commit with working unauthenticated test

---
+++ zenodo-client-tools/src/test/java/io/dockstore/ZenodoClientTest.java
@@ -0,0 +1,20 @@
+package io.dockstore;
+
+import java.util.HashMap;
+
+import io.dockstore.zenodo.client.ApiClient;
+import io.dockstore.zenodo.client.ApiException;
+import io.dockstore.zenodo.client.api.PreviewApi;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ZenodoClientTest {
+
+    @Test
+    public void testZenodoClient() throws ApiException {
+        ApiClient client = new ApiClient();
+        PreviewApi previewApi = new PreviewApi(client);
+        HashMap<String, Object> o = (HashMap<String, Object>)previewApi.listCommunities();
+        Assert.assertTrue("not able to list communities as basic test", o != null && o.keySet().size() > 0);
+    }
+}
